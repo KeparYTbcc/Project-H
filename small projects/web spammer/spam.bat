b94b22eac3571dc49d22b00577c45fde44c646a2
KeparYTbcc <67996155+KeparYTbcc@users.noreply.github.com>
2020-08-31 00:11:34 +0100

    Add files via upload

---
+++ small projects/web spammer/spam.bat
@@ -0,0 +1,17 @@
+@echo off
+color a
+cls
+msg * to stop spam just close cmd
+pause
+cls
+:lol
+start https://diep.io/
+start https://diep.io/
+start https://diep.io/
+start https://diep.io/
+start https://diep.io/
+start https://diep.io/
+start https://diep.io/
+start https://diep.io/
+start https://diep.io/
+goto lol
